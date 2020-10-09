87b12433b5c4637377d45a996da5a7fd5f259a11
winechit-dev <winechitpaing.dev@gmail.com>
2020-10-09 12:01:00 +0630

    explored apollo grachql api

--- data/src/main/java/com/winechitpaing/data/network/SpaceXApi.kt
+++ data/src/main/java/com/winechitpaing/data/network/SpaceXApi.kt
@@ -1,7 +1,7 @@
 package com.winechitpaing.data.network
 
 interface SpaceXApi {
-   companion object{
-       var API_BASE_URL = "https://apollo-fullstack-tutorial.herokuapp.com/"
-   }
+    companion object {
+        var API_BASE_URL = "https://apollo-fullstack-tutorial.herokuapp.com/"
+    }
 }
